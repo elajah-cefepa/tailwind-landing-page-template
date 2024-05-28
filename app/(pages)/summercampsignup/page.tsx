877b67c2c9c96a18ffc1ad633a082919d2a8759f
RunnerStudios <101425155+RunnerStudios@users.noreply.github.com>
2024-05-28 19:06:12 -0400

    s
    s

--- app/(pages)/summercampsignup/page.tsx
+++ app/(pages)/summercampsignup/page.tsx
@@ -54,24 +54,20 @@ export default function SummerCampSignUp() {
   };
 
   return (
-    <form className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          {/* Page header */}
-          
-            <div className="max-w-4xl mx-auto text-center pb-8 md:pb-10">
-  <h1 className="h1">Sign up for Summer Camp</h1>
-  <h1 className="h1">&nbsp;</h1> 
-  <h1 className="h1">by</h1> 
-  <h1 className="h1">&nbsp;</h1> 
-  <h1 className="h1">calling our office at 570-724-4591 or email tiogacounty@cefepa.net to register for summer camp!</h1>
-</div>
-
-    
-
-          
+  <form className="bg-gradient-to-b from-gray-100 to-white">
+    <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        {/* Page header */}
+        
+        <div className="max-w-4xl mx-auto text-center pb-8 md:pb-10">
+          <h1 className="h1" style={{ fontSize: '2rem', margin: '1rem 0' }}>Sign up for Summer Camp</h1>
+          <h1 className="h1" style={{ fontSize: '1.5rem', margin: '1rem 0' }}>by</h1> 
+          <h1 className="h1" style={{ fontSize: '1.5rem', margin: '1rem 0' }}>calling our office at 570-724-4591 or email tiogacounty@cefepa.net </h1>
+          <h1 className="h1" style={{ fontSize: '1.5rem', margin: '1rem 0' }}>to register for summer camp!</h1>
         </div>
       </div>
-    </form>
-  )
+    </div>
+  </form>
+);
+
 }
